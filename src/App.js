13b192397754d24479ee7d452dce188b3167d615
imashiqe <imashiqe@gmail.com>
2023-02-16 00:42:48 +0600

    install  react router dom

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './shared/Header/Header';
 import Footer from './shared/Footer/Footer';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
      <Header></Header>
+     <Outlet></Outlet>
      <Footer></Footer>
     </div>
   );
