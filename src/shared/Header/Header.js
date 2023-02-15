ca836df09a3009e915335cc5949ffa5959db241f
imashiqe <imashiqe@gmail.com>
2023-02-16 00:38:00 +0600

    header menu updated

--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className='bg-blue-900'>
+        <div className='bg-blue-900 sticky top-0 z-30 w-full px-2 py-4  sm:px-4 shadow-xl'>
             <div className='container m-auto'>
             <div className="navbar  bg-blue-900  text-white">
   <div className="navbar-start">
@@ -23,16 +23,25 @@ const Header = () => {
           </ul>
         </li>
         <li><a>Item 3</a></li>
+
+
       </ul>
+
     </div>
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <a className="btn btn-ghost normal-case text-xl">Book Store</a>
+
+    <div className="grid grid-flow-col gap-4">
+      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a> 
+      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a> 
+      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+    </div>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
+      <li><a>Home</a></li>
       <li tabIndex={0}>
         <a>
-          Parent
+          Pages
           <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
         </a>
         <ul className="p-2">
@@ -40,11 +49,16 @@ const Header = () => {
           <li><a>Submenu 2</a></li>
         </ul>
       </li>
-      <li><a>Item 3</a></li>
+      <li><a>About</a></li>
+      <li><a>Services</a></li>
+      <li><a>Contact</a></li>
+      <div className="tooltip tooltip-open tooltip-top" data-tip="15">
+  <button className="btn">Cart</button>
+</div>
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Get started</a>
+    <a className="btn bg-yellow-500 text-blue-900">Order Today</a>
   </div>
 </div>
             </div>
