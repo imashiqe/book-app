9286fabf0def7e3d2655269ac41c7deb0a5da220
imashiqe <imashiqe@gmail.com>
2023-02-15 23:18:46 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './shared/Header/Header';
+import Footer from './shared/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <div className="card w-96 bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+     <Header></Header>
+     <Footer></Footer>
     </div>
   );
 }
