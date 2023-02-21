9ca104cfbd7468a6dfb11e2147e76791f0e5bbe0
imashiqe <imashiqe@gmail.com>
2023-02-22 01:01:01 +0600

    error 404 page added

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import Error404 from "../../component/Error404/Error404";
 import Home from "../../component/Home/Home";
 import Login from "../../component/Login/Login";
 import Register from "../../component/Register/Register";
@@ -23,5 +24,11 @@ import Main from "../../layout/Main";
             element: <Register></Register>
           }
       ]
+    },
+    
+    {
+     path: '*',
+     element: <Error404></Error404>
+
     }
 ]);
