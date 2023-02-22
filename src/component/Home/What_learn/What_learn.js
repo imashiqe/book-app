29d2a2ec9721f3f29ffdecfa9f52fcb137a98ede
imashiqe <imashiqe@gmail.com>
2023-02-23 00:12:51 +0600

    navbar updated

--- src/component/Home/What_learn/What_learn.js
+++ src/component/Home/What_learn/What_learn.js
@@ -11,7 +11,7 @@ const What_learn = () => {
   <div className="card   shadow-xl">
   <div className="card-body">
     <h2 className="card-title bg-yellow-500 rounded-full p-2 mr-10 lg:mr-40 pl-5">01</h2>
-    <p className='text-1xl pt-5'>Use HDFS & Map Reduce for storing & analyzing data at scale.</p>
+    <p className='text-1xl pt-5 text-blue-900'>Use HDFS & Map Reduce for storing & analyzing data at scale.</p>
    
   </div>
 </div>
@@ -19,7 +19,7 @@ const What_learn = () => {
 <div className="card   shadow-xl">
   <div className="card-body">
     <h2 className="card-title  bg-yellow-500 rounded-full p-2 mr-10 lg:mr-40 pl-5">02</h2>
-    <p className='text-1xl pt-5'>Consume streaming data using Spark Streaming, Flink, and Storm.</p>
+    <p className='text-1xl pt-5 text-blue-900'>Consume streaming data using Spark Streaming, Flink, and Storm.</p>
    
   </div>
 </div>
@@ -27,7 +27,7 @@ const What_learn = () => {
 <div className="card   shadow-xl">
   <div className="card-body">
     <h2 className="card-title   bg-yellow-500 rounded-full p-2 mr-10 lg:mr-40 pl-5">03</h2>
-    <p className='text-1xl pt-5'>Choose an appropriate data storage technology for your application</p>
+    <p className='text-1xl pt-5 text-blue-900'>Choose an appropriate data storage technology for your application</p>
    
   </div>
 </div>
@@ -35,7 +35,7 @@ const What_learn = () => {
 <div className="card   shadow-xl">
   <div className="card-body">
     <h2 className="card-title    bg-yellow-500 rounded-full p-2 mr-10 lg:mr-40 pl-5">04</h2>
-    <p  className='text-1xl pt-5'>Analyze non-relational data using HBase, Cassandra, and MongoDB.</p>
+    <p  className='text-1xl pt-5 text-blue-900'>Analyze non-relational data using HBase, Cassandra, and MongoDB.</p>
 
   </div>
 </div>
